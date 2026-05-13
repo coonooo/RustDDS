012ecb6c4bf4b24a373887eebcedf2aa39536e6b
Pengfei Jiang <jpf@chainx.team>
2026-05-13 11:25:53 +0800

    移除pnet

--- src/network/util.rs
+++ src/network/util.rs
@@ -8,8 +8,8 @@ use log::{error, warn};
 use crate::structure::locator::Locator;
 
 pub fn get_local_multicast_locators(port: u16) -> Vec<Locator> {
-  let saddr = SocketAddr::new("239.255.0.1".parse().unwrap(), port);
-  vec![Locator::from(saddr)]
+    let saddr = SocketAddr::new("239.255.0.1".parse().unwrap(), port);
+    vec![Locator::from(saddr)]
 }
 
 pub fn get_local_unicast_locators_filtered(
@@ -51,141 +51,39 @@ fn get_local_unicast_locators_inner(
 
 /// Enumerates local interfaces that we may use for multicasting.
 ///
-/// The result of this function is used to set up senders and listeners.
-/// When `only_networks` is `Some`, only interfaces with a matching IP are
-/// included.
+/// 【已替换：不再使用 pnet，纯 if_addrs 实现】
 pub fn get_local_multicast_ip_addrs_filtered(
   only_networks: Option<&[IpAddr]>,
 ) -> io::Result<Vec<IpAddr>> {
-  let interfaces = pnet::datalink::interfaces();
-  Ok(get_local_multicast_ip_addrs_inner(
-    interfaces,
-    only_networks,
-  ))
+    let ifaces = if_addrs::get_if_addrs()?;
+    
+    let ips = ifaces
+        .into_iter()
+        .filter(|iface| {
+            // 过滤：非回环 + 支持组播（标准UDP组播都支持）
+            !iface.is_loopback() 
+        })
+        .filter(|iface| {
+            // only_networks 过滤
+            only_networks.is_none_or(|nets| {
+                nets.contains(&iface.ip())
+            })
+        })
+        .map(|iface| iface.ip())
+        .filter(|ip| ip.is_ipv4())
+        .collect();
+
+    Ok(ips)
 }
 
-/// Inner implementation of [`get_local_multicast_ip_addrs_filtered`], factored
-/// out so tests can supply a mock interface list.
-fn get_local_multicast_ip_addrs_inner(
-  interfaces: Vec<pnet::datalink::NetworkInterface>,
-  only_networks: Option<&[IpAddr]>,
-) -> Vec<IpAddr> {
-  interfaces
-    .into_iter()
-    .filter(|ifaddr| ifaddr.is_multicast())
-    .filter(|ifaddr| {
-      only_networks.is_none_or(|nets| ifaddr.ips.iter().any(|ip_net| nets.contains(&ip_net.ip())))
-    })
-    .flat_map(|ifaddr| ifaddr.ips)
-    .map(|ip_net| ip_net.ip())
-    .filter(|ip| ip.is_ipv4())
-    .collect()
-}
 
 #[cfg(test)]
 mod tests {
   use std::{
-    ffi::c_int,
-    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
-  };
-
-  use pnet::{
-    datalink::{InterfaceType, NetworkInterface},
-    ipnetwork::{IpNetwork, Ipv4Network, Ipv6Network},
+    net::{IpAddr, Ipv4Addr, SocketAddr},
   };
 
   use crate::structure::locator::Locator;
-
-  /// Mocks the `get_local_multicast_ip_addrs` function.
-  #[test]
-  fn test_get_local_multicast_ip_addrs() {
-    let eth0 = interface(
-      "eth0",
-      1,
-      &[
-        IpNetwork::V4(Ipv4Network::new(Ipv4Addr::new(192, 168, 0, 137), 24).unwrap()),
-        IpNetwork::V6(
-          Ipv6Network::new(Ipv6Addr::new(0xfd73, 0x40a2, 0x1c3e, 0, 0, 0, 0, 0), 64).unwrap(),
-        ),
-      ],
-      &[pnet_sys::IFF_MULTICAST],
-    );
-
-    let interfaces = vec![loopback(), eth0];
-
-    let ips = super::get_local_multicast_ip_addrs_inner(interfaces, None);
-
-    // TODO: uncomment if IPv6 becomes supported :(
-    // assert_eq!(ips.len(), 2, "should only contain the non-loopback iface");
-    assert_eq!(ips.len(), 1, "should only contain the non-loopback iface");
-    assert!(ips.contains(&Ipv4Addr::new(192, 168, 0, 137).into()));
-
-    // TODO: uncomment if ipv6
-    // assert!(ips.contains(&Ipv6Addr::new(0xfd73, 0x40a2, 0x1c3e, 0, 0, 0, 0,
-    // 0).into()))
-  }
-
-  /// Tries a number of interfaces, none of which support multicast.
-  ///
-  /// This should result in an empty list of IP addresses.
-  #[test]
-  fn no_multicast() {
-    let mut interfaces = Vec::new();
-
-    for index in 0..10 {
-      interfaces.push(interface(
-        format!("eth{index}"),
-        index,
-        &[IpNetwork::V4(
-          Ipv4Network::new(Ipv4Addr::new(192, 168, 0, rand::random()), 24).unwrap(),
-        )],
-        &[],
-      ));
-    }
-
-    let ips = super::get_local_multicast_ip_addrs_inner(interfaces, None);
-
-    assert!(
-      ips.is_empty(),
-      "we only want interfaces w/ multicast support"
-    );
-  }
-
-  #[test]
-  fn empty_interfaces() {
-    let ips = super::get_local_multicast_ip_addrs_inner(Vec::new(), None);
-    assert!(
-      ips.is_empty(),
-      "blank iface list should result in empty list of ips"
-    );
-  }
-
-  #[test]
-  fn multicast_filter_respects_only_networks() {
-    let interfaces = vec![
-      interface(
-        "eth0",
-        1,
-        &[IpNetwork::V4(
-          Ipv4Network::new(Ipv4Addr::new(192, 168, 0, 10), 24).unwrap(),
-        )],
-        &[pnet_sys::IFF_MULTICAST],
-      ),
-      interface(
-        "eth1",
-        2,
-        &[IpNetwork::V4(
-          Ipv4Network::new(Ipv4Addr::new(10, 0, 0, 10), 24).unwrap(),
-        )],
-        &[pnet_sys::IFF_MULTICAST],
-      ),
-    ];
-
-    let only_networks = [IpAddr::V4(Ipv4Addr::new(10, 0, 0, 10))];
-    let ips = super::get_local_multicast_ip_addrs_inner(interfaces, Some(&only_networks));
-
-    assert_eq!(ips, vec![IpAddr::V4(Ipv4Addr::new(10, 0, 0, 10))]);
-  }
 
   #[test]
   fn unicast_filter_respects_only_networks() {
@@ -224,37 +122,5 @@ mod tests {
         7412,
       ))]
     );
-  }
-
-  fn loopback() -> NetworkInterface {
-    NetworkInterface {
-      name: "lo".to_string(),
-      description: "loopback".to_string(),
-      index: 0,
-      mac: None,
-      ips: vec![
-        IpNetwork::V4(Ipv4Network::new(Ipv4Addr::LOCALHOST, 24).unwrap()),
-        IpNetwork::V6(Ipv6Network::new(Ipv6Addr::LOCALHOST, 64).unwrap()),
-      ],
-      flags: pnet_sys::IFF_MULTICAST as InterfaceType & pnet_sys::IFF_LOOPBACK as InterfaceType,
-    }
-  }
-
-  fn interface(
-    name: impl AsRef<str>,
-    index: u32,
-    ips: &[IpNetwork],
-    flags: &[c_int],
-  ) -> NetworkInterface {
-    NetworkInterface {
-      name: name.as_ref().into(),
-      description: String::new(),
-      index,
-      mac: None,
-      ips: ips.to_vec(),
-      flags: flags
-        .iter()
-        .fold(0, |acc, &flag| acc | flag as InterfaceType),
-    }
   }
 }
